79c834bd00a3da1138eb6c89f17f5d3261c76884
Abhimanyu Dwivedi <AbhimanyuDwivedi@outlook.com>
2018-01-20 21:07:14 +0530

    Add files via upload
    A simple Rock Paper Scissors game!

---
+++ Rock_Paper_Scissors.py
@@ -0,0 +1,40 @@
+loop=10
+while(loop>1):
+ print('\n\n')
+ print("Python Rock Paper Scissors Game")
+ print("Remember the rules:\n1.Rock beats scissors \n2.Scissors beats paper \n3.Paper beats rock")
+ import random
+ choice2 = random.randint(1, 3)
+ choice1 = int(input("Enter your choice as 1 for Rock, 2 for Scissors and 3 for Paper:"))
+ if(choice2==1):
+  print("Computer choice- Rock")
+ if(choice2==2):
+  print("Computer choice- Scissors")
+ if(choice2==3):
+  print("Computer choice- Paper")
+ if(choice1==1):
+  print("Your choice- Rock")
+ if(choice1==2):
+  print("Your choice- Scissors")
+ if(choice1==3):
+  print("Your choice- Paper")
+ if (choice1 == choice2):
+     print("Opps! Same choice. It's a tie")
+ elif (choice1 == 1):
+         if (choice2 == 2):
+           print("Rock wins")
+         else :
+                 print("Scissors wins")
+ elif (choice1 == 2):
+         if (choice2 == 3):
+             print("Scissors wins")
+         else :
+                 print("Paper wins")
+
+ elif (choice1 == 3):
+        if (choice2 == 1):
+            print("Paper wins")
+        else :
+                print("Rock wins")
+ print('----------------------------------------------------------')
+ print('----------------------------------------------------------')
